896bbebfb7863e41a78a5e1996b533a2d3deb4fc
Takumi Endo <tq-ed@v7.com>
2015-01-29 00:19:39 +0900

    first commit.

---
+++ init.rb
@@ -0,0 +1,9 @@
+Redmine::Plugin.register :redmine_custom_notification do
+  name 'Redmine Custom Notification plugin'
+  author 'Takumi Endo'
+  description 'A plugin for Redmine that sends email using user-specified fields and recipients.' 
+  version '0.0.1'
+  url 'https://github.com/ihybs/redmine_custom_notification'
+
+  requires_redmine :version_or_higher => '2.4.0'
+end
