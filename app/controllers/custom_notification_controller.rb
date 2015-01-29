ea86a25a77f1beab04d469d4b69c9145d08e7402
Takumi Endo <tq-ed@v7.com>
2015-01-29 13:13:42 +0900

    Add simple preview functionality when sending mail.

--- app/controllers/custom_notification_controller.rb
+++ app/controllers/custom_notification_controller.rb
@@ -1,16 +1,30 @@
 # coding: utf-8
 class CustomNotificationController < ApplicationController
   unloadable
+  before_filter :find_issue
 
   def new
-    @issue = Issue.find(params[:issue_id])
     @notification_template = CustomNotificationTemplate.new
     @notification_templates = CustomNotificationTemplate.all
   end
 
+  def select
+    if params[:custom_notification_template_id].present?
+      @notification_template = CustomNotificationTemplate.find(params[:custom_notification_template_id])
+    else
+      @notification_template = CustomNotificationTemplate.new
+    end
+    @notification_templates = CustomNotificationTemplate.all
+  end
+
   def send_mail
-    @issue = Issue.find(params[:issue_id])
     @notification_template = CustomNotificationTemplate.find(params[:custom_notification_template][:selected_notification_template])
     CustomNotificationMailer.deliver_issue_by_template(@issue, @notification_template)
+  end
+
+  private
+
+  def find_issue
+    @issue = Issue.find(params[:issue_id])
   end
 end
